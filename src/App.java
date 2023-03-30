155cb92cf9d6363cc3f26833cb1921047f6ae453
masilvafla <masilva.fla@gmail.com>
2023-03-30 00:45:50 -0300

    Comitando Aula 2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -22,7 +24,22 @@ public class App {
         List<Map<String, String>> listaDeFilmes = jsonParser.parse(body);
 
         // exibir e manipular os dados
-        for (int i = 0; i < 3; i++ ) {
+
+        for (Map<String,String> filme : listaDeFilmes) {
+            String titulo = filme.get("title");
+            String rating = filme.get("imDbRating");
+            String urlImage = filme.get("image");
+
+            InputStream inputStream = new URL(urlImage).openStream();
+            String nomeArquivo = titulo + ".png";
+            var geradora = new GeradoraDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo);
+            System.out.println(titulo);
+            System.out.println();
+        }
+       
+/*        
+        for (int i = 0; i < 6; i++ ) {
             Map<String,String> filme = listaDeFilmes.get(i);
                 System.out.println("\u001b[1mTitulo:\u001b[m " + filme.get("title"));
                 System.out.println("\u001b[1mURL da Imagem:\u001b[m " + filme.get("image"));
@@ -33,6 +50,7 @@ public class App {
                 }
             System.out.println();
         }
+*/ 
 
     }
 }
